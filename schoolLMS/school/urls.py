d3c95092b2a194ed9b877463465e2c78f27421b2
Saja Abdullah <sajood6667@gmail.com>
2022-11-09 14:41:14 +0500

    #REFAC

--- schoolLMS/school/urls.py
+++ schoolLMS/school/urls.py
@@ -2,29 +2,21 @@ from django.urls import path
 
 from .views import (
     TeacherCreateGenaricView,
+    TeacherDeleteGenaricView,
     TeacherListGenaricView,
     TeacherRetrieveGenaricView,
-    TeacherDeleteGenaricView,
-    TeacherUpdateGenaricView
+    TeacherUpdateGenaricView,
 )
 
 app_name = "school"
 urlpatterns = [
-    path(
-        "list_teachers/", 
-        TeacherListGenaricView.as_view(),
-        name="teachers_list_api"
-    ),
+    path("list_teachers/", TeacherListGenaricView.as_view(), name="teachers_list_api"),
     path(
         "retrieve_teacher/<uuid:id>",
         TeacherRetrieveGenaricView.as_view(),
         name="retrieve_teacher",
     ),
-    path(
-        "create_teacher/",
-        TeacherCreateGenaricView.as_view(),
-        name="create_teacher"
-    ),
+    path("create_teacher/", TeacherCreateGenaricView.as_view(), name="create_teacher"),
     path(
         "update_teacher/<uuid:id>",
         TeacherUpdateGenaricView.as_view(),
--- schoolLMS/school/views.py
+++ schoolLMS/school/views.py
@@ -29,21 +29,26 @@ class TeacherCreateGenaricView(generics.CreateAPIView):
     """
     Save teacher object in DB
     """
+
     queryset = Teacher.objects.all()
     serializer_class = UserSerializer
+
 
 class TeacherUpdateGenaricView(generics.UpdateAPIView):
     """
     update teacher acoording givien data
     """
+
     queryset = Teacher.objects.all()
     serializer_class = UserSerializer
     lookup_field = "id"
+
 
 class TeacherDeleteGenaricView(generics.DestroyAPIView):
     """
     update teacher acoording givien data
     """
+
     queryset = Teacher.objects.all()
     serializer_class = UserSerializer
     lookup_field = "id"
