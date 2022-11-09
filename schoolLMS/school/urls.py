192984f721bd514e7ee1e323a1152cf251f48238
Saja Abdullah <sajood6667@gmail.com>
2022-11-09 14:14:33 +0500

    #FEAT Create Teacher

--- schoolLMS/school/urls.py
+++ schoolLMS/school/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from .views import TeacherRetrieveGenaricView, TeacherListGenaricView
+from .views import (
+    TeacherCreateGenaricView,
+    TeacherListGenaricView,
+    TeacherRetrieveGenaricView,
+)
 
 app_name = "school"
 urlpatterns = [
     path("list_teachers/", TeacherListGenaricView.as_view(), name="teachers_list_api"),
-    path("retrieve_teacher/<uuid:id>", TeacherRetrieveGenaricView.as_view(),name="retrieve_teacher"),
-] 
+    path(
+        "retrieve_teacher/<uuid:id>",
+        TeacherRetrieveGenaricView.as_view(),
+        name="retrieve_teacher",
+    ),
+    path("create/", TeacherCreateGenaricView.as_view(), name="create_teacher"),
+]
--- schoolLMS/school/views.py
+++ schoolLMS/school/views.py
@@ -1,5 +1,4 @@
-from rest_framework import status
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -11,14 +10,24 @@ class TeacherListGenaricView(generics.ListAPIView):
     """
     using genaric views list teachers
     """
+
     queryset = Teacher.objects.all()
     serializer_class = UserSerializer
+
 
 class TeacherRetrieveGenaricView(generics.RetrieveAPIView):
     """
     using genaric RetrieveAPIView retrive teacher
     """
+
     queryset = Teacher.objects.all()
     serializer_class = UserSerializer
-    lookup_field="id"
-   
+    lookup_field = "id"
+
+
+class TeacherCreateGenaricView(generics.CreateAPIView):
+    """
+    Save teacher object in DB
+    """
+    queryset = Teacher.objects.all()
+    serializer_class = UserSerializer
